1f5218dc777b27adcea12e2db33da81aedcfc8a3
matthieu ROLLA <matthieu.rolla@c3iot.com>
2019-04-05 11:02:13 +0200

    change to be able to set by route

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const FJS = require('fast-json-stringify');
-const ms = require('ms');
+const fp = require('fastify-plugin')
+const FJS = require('fast-json-stringify')
+const ms = require('ms')
 
 const LocalStore = require('./store/LocalStore')
 const RedisStore = require('./store/RedisStore')
@@ -28,27 +29,27 @@ function rateLimitPlugin (opts) {
 
   const keyGenerator = typeof opts.keyGenerator === 'function'
     ? opts.keyGenerator
-    : (req) => req.ip;
+    : (req) => req.ip
 
-  const skipOnError = opts.skipOnError === true;
+  const skipOnError = opts.skipOnError === true
   const max = opts.max || 1000
-  const whitelist = opts.whitelist || [];
-
-  const after = ms(timeWindow, { long: true });
+  const whitelist = opts.whitelist || []
+  const after = ms(timeWindow, { long: true })
 
   function rateLimit (req, res, next) {
     var key = keyGenerator(req)
+
     if (whitelist.indexOf(key) > -1) {
       next()
     } else {
-      store.incr(key, onIncr)
+      store.incr(`${key}-${req.originalUrl}`, timeWindow, onIncr)
     }
 
     function onIncr (err, current) {
       if (err && skipOnError === false) return next(err)
 
       if (current <= max) {
-        res.setHeader('X-RateLimit-Limit', max);
+        res.setHeader('X-RateLimit-Limit', max)
         res.setHeader('X-RateLimit-Remaining', max - current)
         next()
       } else {
@@ -56,14 +57,14 @@ function rateLimitPlugin (opts) {
           statusCode: 429,
           error: 'Too Many Requests',
           message: `Rate limit exceeded, retry in ${after}`
-        });
+        })
         res.writeHead(429, {
-          'Content-Type' : 'application/json',
+          'Content-Type': 'application/json',
           'Content-Length': Buffer.byteLength(body),
-          'X-RateLimit-Limit' : max,
-          'X-RateLimit-Remaining' : 0,
-          'Retry-After' : timeWindow
-        }).end(body);
+          'X-RateLimit-Limit': max,
+          'X-RateLimit-Remaining': 0,
+          'Retry-After': timeWindow
+        }).end(body)
       }
     }
   }
@@ -71,4 +72,7 @@ function rateLimitPlugin (opts) {
   return rateLimit
 }
 
-module.exports = rateLimitPlugin;
+module.exports = fp(rateLimitPlugin, {
+  fastify: '>=2.x',
+  name: 'fastify-rate-limit'
+})
