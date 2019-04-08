da641f5e22e60ef4ab556abe607d27acb39d4720
matthieu ROLLA <matthieu.rolla@c3iot.com>
2019-04-08 04:41:30 +0200

    add global mode on/off + refactor of the way the plugin generate the preHandler

--- index.js
+++ index.js
@@ -14,137 +14,150 @@ const serializeError = FJS({
   }
 })
 
-function rateLimitPlugin (fastify, rule, next) {
-  if (rule.whiteListInRedis && !rule.redis) {
+function buildRouteRate(pluginComponent, params, routeOptions) {
+
+  const urlT = (routeOptions.url === '/') ? 'root' : routeOptions.url.replace(/\//g, ':').slice(1)
+
+  const prefix = {
+    cache: params.prefixCache ? params.prefixCache.replace(/[-,.;#*]/g, ':') : urlT
+  }
+  if (pluginComponent.isRedis && pluginComponent.whiteListInRedis) {
+    prefix.wl = urlT
+    pluginComponent.store.addWhiteList(`${pluginComponent.app}:wle:${prefix.wl}`, params.whitelist, (err) =>{
+      if (err && params.skipOnError === false) return next(err)
+    })
+  } else {
+    pluginComponent.whitelist.endpoint[prefix.cache] = params.whitelist || []
+  }
+
+  routeOptions.preHandler = (req, res, next) => {
+    var key = params.keyGenerator(req)
+
+    if (pluginComponent.isRedis) {
+      if (pluginComponent.whiteListInRedis) {
+        pluginComponent.store.isWhiteList(`${pluginComponent.app}:wlg`, key, function ([err, result]) {
+          if (err && params.skipOnError === false) return next(err)
+          if (result === 1) {
+            next()
+          }
+        })
+        pluginComponent.store.isWhiteList(`${pluginComponent.app}:wle:${prefix.wl}`, key, function ([err, result]) {
+          if (err && params.skipOnError === false) return next(err)
+          if (result === 1) {
+            next()
+          }
+        })
+      }
+    } else {
+      if (pluginComponent.whitelist.global.indexOf(key) > -1 || pluginComponent.whitelist.endpoint[prefix.cache].indexOf(key) > -1) {
+        next()
+      }
+    }
+
+    pluginComponent.store.incr(prefix.cache, key, params.globalTimeWindow, onIncr)
+
+    function onIncr (err, current) {
+      if (err && params.skipOnError === false) return next(err)
+
+      if (current <= params.max) {
+        res.header('X-RateLimit-Limit', params.max)
+        res.header('X-RateLimit-Remaining', params.max - current)
+
+        if (typeof params.onExceeding === 'function') {
+          params.onExceeding(req)
+        }
+
+        next()
+      } else {
+        if (typeof params.onExceeded === 'function') {
+          params.onExceeded(req)
+        }
+
+        res.type('application/json').serializer(serializeError)
+        res.code(429)
+          .header('X-RateLimit-Limit', params.max)
+          .header('X-RateLimit-Remaining', 0)
+          .header('Retry-After', params.globalTimeWindow)
+          .send({
+            statusCode: 429,
+            error: 'Too Many Requests',
+            message: `Rate limit exceeded, retry in ${params.after}`
+          })
+      }
+    }
+  }
+}
+
+function rateLimitPlugin (fastify, settings, next) {
+  if (settings.whiteListInRedis && !settings.redis) {
     next('you must set redis parameter to use it')
   }
 
-  const max = (typeof rule.max === 'number' || typeof rule.max === 'function')
-    ? rule.max
+  const globalParams = {
+    global : settings.global || true
+  }
+
+  globalParams.max = (typeof settings.max === 'number' || typeof settings.max === 'function')
+    ? settings.max
     : 1000
-  const globalTimeWindow = typeof rule.timeWindow === 'string'
-    ? ms(rule.timeWindow)
-    : typeof rule.timeWindow === 'number'
-      ? rule.timeWindow
+
+  globalParams.globalTimeWindow = typeof settings.timeWindow === 'string'
+    ? ms(settings.timeWindow)
+    : typeof settings.timeWindow === 'number'
+      ? settings.timeWindow
       : 1000 * 60
 
-  const plugin = {
-    app: rule.appName || 'default'
+  const pluginComponent = {
+    app: settings.appName || 'default'
   }
 
-  const skipOnError = rule.skipOnError === true
+  globalParams.skipOnError = settings.skipOnError === true
 
-  function cbR (err) {
-    if (err && skipOnError === false) return next(err)
-  }
-
-  if (rule.redis) {
-    plugin.store = new RedisStore(rule.redis)
-    if (rule.whiteListInRedis) {
-      plugin.store.addWhiteList(`${plugin.app}:wlg`, rule.whitelist || [], cbR)
-      plugin.isRedis = true
-      plugin.whiteListInRedis = true
+  if (settings.redis) {
+    pluginComponent.store = new RedisStore(settings.redis)
+    if (settings.whiteListInRedis) {
+      pluginComponent.store.addWhiteList(`${pluginComponent.app}:wlg`, settings.whitelist || [], (err) => {
+        if (err && globalParams.skipOnError === false) return next(err)
+      })
+      pluginComponent.isRedis = true
+      pluginComponent.whiteListInRedis = true
     } else {
-      plugin.whitelist = {
-        global: rule.whitelist || [],
+      pluginComponent.whitelist = {
+        global: settings.whitelist || [],
         endpoint: []
       }
-      plugin.isRedis = true
-      plugin.whiteListInRedis = false
+      pluginComponent.isRedis = true
+      pluginComponent.whiteListInRedis = false
     }
   } else {
-    plugin.store = new LocalStore(rule.cache)
-    plugin.isRedis = false
-    plugin.whiteListInRedis = false
-    plugin.whitelist = {
-      global: rule.whitelist || [],
+    pluginComponent.store = new LocalStore(settings.cache)
+    pluginComponent.isRedis = false
+    pluginComponent.whiteListInRedis = false
+    pluginComponent.whitelist = {
+      global: settings.whitelist || [],
       endpoint: {}
     }
   }
 
-  const after = ms(globalTimeWindow, { long: true })
+  globalParams.after = ms(globalParams.globalTimeWindow, { long: true })
 
-  const keyGenerator = typeof rule.keyGenerator === 'function'
-    ? rule.keyGenerator
+  globalParams.keyGenerator = typeof settings.keyGenerator === 'function'
+    ? settings.keyGenerator
     : (req) => req.raw.ip
 
+  const makeParams = (p) => { return {...globalParams, ...p}};
+
   fastify.addHook('onRoute', (routeOptions) => {
-    if (routeOptions.config && routeOptions.config.rateLimit && typeof routeOptions.config.rateLimit === 'object') {
-      const params = routeOptions.config.rateLimit
 
-      if (!params.max) {
-        params.max = max
-      }
-
-      const urlT = (routeOptions.url === '/') ? 'root' : routeOptions.url.replace(/\//g, ':').slice(1)
-
-      const prefix = {
-        cache: params.prefixCache ? params.prefixCache.replace(/[-,.;#*]/g, ':') : urlT
-      }
-      if (plugin.isRedis && plugin.whiteListInRedis) {
-        prefix.wl = urlT
-        plugin.store.addWhiteList(`${plugin.app}:wle:${prefix.wl}`, params.whitelist, cbR)
+    if(globalParams.global) {
+      if (routeOptions.config && routeOptions.config.rateLimit && typeof routeOptions.config.rateLimit === 'object') {
+        buildRouteRate(pluginComponent, makeParams(routeOptions.config.rateLimit), routeOptions)
       } else {
-        plugin.whitelist.endpoint[prefix.cache] = params.whitelist || []
+        buildRouteRate(pluginComponent, globalParams, routeOptions)
       }
-
-      routeOptions.preHandler = (req, res, next) => {
-        var key = keyGenerator(req)
-
-        console.log(plugin)
-
-        if (plugin.isRedis) {
-          if (plugin.whiteListInRedis) {
-            plugin.store.isWhiteList(`${plugin.app}:wlg`, key, function ([err, result]) {
-              if (err && skipOnError === false) return next(err)
-              if (result === 1) {
-                next()
-              }
-            })
-            plugin.store.isWhiteList(`${plugin.app}:wle:${prefix.wl}`, key, function ([err, result]) {
-              if (err && skipOnError === false) return next(err)
-              if (result === 1) {
-                next()
-              }
-            })
-          }
-        } else {
-          if (plugin.whitelist.global.indexOf(key) > -1 || plugin.whitelist.endpoint[prefix.cache].indexOf(key) > -1) {
-            next()
-          }
-        }
-
-        plugin.store.incr(prefix.cache, key, globalTimeWindow, onIncr)
-
-        function onIncr (err, current) {
-          if (err && skipOnError === false) return next(err)
-
-          if (current <= params.max) {
-            res.header('X-RateLimit-Limit', params.max)
-            res.header('X-RateLimit-Remaining', params.max - current)
-
-            if (typeof params.onExceeding === 'function') {
-              params.onExceeding(req)
-            }
-
-            next()
-          } else {
-            if (typeof params.onExceeded === 'function') {
-              params.onExceeded(req)
-            }
-
-            res.type('application/json').serializer(serializeError)
-            res.code(429)
-              .header('X-RateLimit-Limit', max)
-              .header('X-RateLimit-Remaining', 0)
-              .header('Retry-After', globalTimeWindow)
-              .send({
-                statusCode: 429,
-                error: 'Too Many Requests',
-                message: `Rate limit exceeded, retry in ${after}`
-              })
-          }
-        }
+    } else {
+      if (routeOptions.config && routeOptions.config.rateLimit && typeof routeOptions.config.rateLimit === 'object') {
+        buildRouteRate(pluginComponent, makeParams(routeOptions.config.rateLimit), routeOptions)
       }
     }
   })
