302d4f36793bd1231bef44c1237c7c389f4ee330
Max <rtwatmi@gmail.com>
2019-11-29 23:25:16 +0300

    ts types test update

--- test/types/types.test.ts
+++ test/types/types.test.ts
@@ -1,11 +1,11 @@
 import * as http from 'http'
+import * as http2 from 'http2'
 import * as fastify from 'fastify';
 import * as fastifyRateLimit from '../../../fastify-rate-limit';
 import * as ioredis from 'ioredis';
 
-const app = fastify();
-
-app.register(fastifyRateLimit, {
+const appWithImplicitHttp = fastify()
+const options1 = {
   global: true,
   max: 3,
   timeWindow: 5000,
@@ -16,11 +16,23 @@ app.register(fastifyRateLimit, {
   ban: 10,
   keyGenerator: (req: fastify.FastifyRequest<http.IncomingMessage>) => req.ip,
   errorResponseBuilder: (req: fastify.FastifyRequest<http.IncomingMessage>, context: fastifyRateLimit.errorResponseBuilderContext) => ({ code: 429, timeWindow: context.after, limit: context.max })
-});
+}
 
-app.register(fastifyRateLimit, {
+const options2 = {
   global: true,
   max: (req: fastify.FastifyRequest<http.IncomingMessage>, key: string) => (42),
   whitelist: (req: fastify.FastifyRequest<http.IncomingMessage>, key: string) => (false),
   timeWindow: 5000
-});
+}
+
+appWithImplicitHttp.register(fastifyRateLimit, options1)
+appWithImplicitHttp.register(fastifyRateLimit, options2)
+
+const appWithHttp2: fastify.FastifyInstance<
+  http2.Http2Server,
+  http2.Http2ServerRequest,
+  http2.Http2ServerResponse
+> = fastify({ http2: true })
+
+appWithHttp2.register(fastifyRateLimit, options1)
+appWithHttp2.register(fastifyRateLimit, options2)
