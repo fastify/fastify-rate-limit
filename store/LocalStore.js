708cc79c3490930ba1a58f50b9995265af0b46a2
Raz Luvaton <rluvaton@users.noreply.github.com>
2022-02-13 18:21:48 +0200

    refactor(local-store): updated tiny-lru to version 8.0.0 and remove the use of the secret param (bypass)

--- store/LocalStore.js
+++ store/LocalStore.js
@@ -12,14 +12,10 @@ function LocalStore (timeWindow, cache, app, continueExceeding) {
 LocalStore.prototype.incr = function (ip, cb) {
   const nowInMs = Date.now()
   const current = this.lru.get(ip) || { count: 0, iterationStartMs: nowInMs }
-  const isNewItem = current.count === 0
 
   current.count++
 
-  // We want to update the TTL only if it's a new item (because it doesn't have any TTL)
-  // Passing false to the 3rd parameter = update the TTL AND I don't know if the item with that key already exist
-  // Passing true means don't update the TTL AND I'm sure there is an item with that key
-  this.lru.set(ip, current, !isNewItem)
+  this.lru.set(ip, current)
 
   if (this.continueExceeding) {
     cb(null, { current: current.count, ttl: this.timeWindow })
