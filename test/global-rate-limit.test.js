5f28dc9d21010f84641f4254a1ce38b2794c5a1e
uzlopak <aras.abbasi@googlemail.com>
2022-09-14 11:47:18 +0200

    add allowList else case unit test

--- test/global-rate-limit.test.js
+++ test/global-rate-limit.test.js
@@ -149,7 +149,7 @@ test('When passing NaN to the timeWindow property then the timeWindow should be 
   })
 })
 
-test('With ips allowList', async t => {
+test('With ips allowList, allowed ips should not result in rate limitting', async t => {
   t.plan(3)
   const fastify = Fastify()
   await fastify.register(rateLimit, {
@@ -170,6 +170,29 @@ test('With ips allowList', async t => {
 
   res = await fastify.inject('/')
   t.equal(res.statusCode, 200)
+})
+
+test('With ips allowList, not allowed ips should result in rate limitting', async t => {
+  t.plan(3)
+  const fastify = Fastify()
+  await fastify.register(rateLimit, {
+    max: 2,
+    timeWindow: '2s',
+    allowList: ['1.1.1.1']
+  })
+
+  fastify.get('/', async (req, reply) => 'hello!')
+
+  let res
+
+  res = await fastify.inject('/')
+  t.equal(res.statusCode, 200)
+
+  res = await fastify.inject('/')
+  t.equal(res.statusCode, 200)
+
+  res = await fastify.inject('/')
+  t.equal(res.statusCode, 429)
 })
 
 test('With ips whitelist', async t => {
