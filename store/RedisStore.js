daa8cdd5e1eaf5f8f1cde6362b60b7945dda6bf2
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-10-24 14:13:42 +0200

    align stores

--- store/RedisStore.js
+++ store/RedisStore.js
@@ -1,51 +1,50 @@
 'use strict'
 
-const noop = () => { }
-
-function RedisStore (redis, key, timeWindow, continueExceeding) {
+function RedisStore (redis, timeWindow, continueExceeding, key) {
   this.redis = redis
   this.timeWindow = timeWindow
-  this.key = key
   this.continueExceeding = continueExceeding
+  this.key = key
 }
 
-RedisStore.prototype.incr = function (ip, cb) {
+RedisStore.prototype.incr = function (ip, cb, max) {
   const key = this.key + ip
-  if (this.continueExceeding) {
-    this.redis.pipeline()
-      .incr(key)
-      .pexpire(key, this.timeWindow)
-      .exec((err, result) => {
-        if (err) return cb(err, { current: 0 })
-        if (result[0][0]) return cb(result[0][0], { current: 0 })
+
+  this.redis.pipeline()
+    .incr(key)
+    .pttl(key)
+    .exec((err, result) => {
+    /**
+     * result[0] => incr response: [0]: error, [1]: new incr value
+     * result[1] => pttl response: [0]: error, [1]: ttl remaining
+     */
+      if (err || result[0][0] || result[1][0]) {
+        cb(err || result[0][0] || result[1][0], { current: 1, ttl: this.timeWindow })
+        return
+      }
+
+      if (result[1][1] === -1) {
+        // Item just got created
+        this.redis.pexpire(key, this.timeWindow, noop)
+        cb(null, { current: 1, ttl: this.timeWindow })
+        return
+      }
+
+      if (this.continueExceeding && result[0][1] > max) {
+        // Reset TLL if max has been exceeded and `continueExceeding` is enabled
+        this.redis.pexpire(key, this.timeWindow, noop)
         cb(null, { current: result[0][1], ttl: this.timeWindow })
-      })
-  } else {
-    this.redis.pipeline()
-      .incr(key)
-      .pttl(key)
-      .exec((err, result) => {
-        /**
-         * result[0] => incr response: [0]: error, [1]: new incr value
-         * result[1] => pttl response: [0]: error, [1]: ttl remaining
-         */
-        if (err) return cb(err, { current: 0 })
-        if (result[0][0]) return cb(result[0][0], { current: 0 })
-        if (result[1][1] === -1) {
-          this.redis.pexpire(key, this.timeWindow, noop)
-          result[1][1] = this.timeWindow
-        }
-        cb(null, { current: result[0][1], ttl: result[1][1] })
-      })
-  }
+        return
+      }
+
+      cb(null, { current: result[0][1], ttl: result[1][1] })
+    })
 }
 
 RedisStore.prototype.child = function (routeOptions) {
-  const child = Object.create(this)
-  child.key = this.key + routeOptions.routeInfo.method + routeOptions.routeInfo.url + '-'
-  child.timeWindow = routeOptions.timeWindow
-  child.continueExceeding = routeOptions.continueExceeding
-  return child
+  return new RedisStore(this.redis, routeOptions.timeWindow, routeOptions.continueExceeding, this.key + routeOptions.routeInfo.method + routeOptions.routeInfo.url + '-')
 }
+
+function noop () {}
 
 module.exports = RedisStore
