7b39166a4d9a2378b65e12abf044ed536397f49f
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-10-25 22:45:41 +0200

    enable autopipeline

--- store/RedisStore.js
+++ store/RedisStore.js
@@ -8,6 +8,31 @@ function RedisStore (redis, timeWindow, continueExceeding, key) {
 }
 
 RedisStore.prototype.incr = function (ip, cb, max) {
+  if (this.redis.options.enableAutoPipelining) this.incrAutoPipe(ip, cb, max)
+  else this.incrNormal(ip, cb, max)
+}
+
+RedisStore.prototype.incrAutoPipe = async function (ip, cb, max) {
+  const key = this.key + ip
+  const [current, ttl] = await Promise.all([this.redis.incr(key), this.redis.pttl(key)])
+
+  if (ttl === -1) {
+    // Item just got created
+    this.redis.pexpire(key, this.timeWindow).catch()
+    cb(null, { current: 1, ttl: this.timeWindow })
+    return
+  }
+
+  if (this.continueExceeding && current > max) {
+    // Reset TLL if max has been exceeded and `continueExceeding` is enabled
+    this.redis.pexpire(key, this.timeWindow).catch()
+    cb(null, { current, ttl: this.timeWindow })
+  }
+
+  return cb(null, { current, ttl })
+}
+
+RedisStore.prototype.incrNormal = function (ip, cb, max) {
   const key = this.key + ip
 
   this.redis.pipeline()
@@ -19,7 +44,7 @@ RedisStore.prototype.incr = function (ip, cb, max) {
      * result[1] => pttl response: [0]: error, [1]: ttl remaining
      */
       if (err || result[0][0] || result[1][0]) {
-        cb(err || result[0][0] || result[1][0], { current: 1, ttl: this.timeWindow })
+        cb(err || result[0][0] || result[1][0], null)
         return
       }
 
