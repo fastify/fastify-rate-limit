81a19ff10eb8e01d4349384bd9be40c9bf4efa60
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-10-25 23:15:03 +0200

    make them async

--- store/RedisStore.js
+++ store/RedisStore.js
@@ -33,44 +33,37 @@ RedisStore.prototype.incrAutoPipe = async function (ip, cb, max) {
   cb(null, { current, ttl })
 }
 
-RedisStore.prototype.incrNormal = function (ip, cb, max) {
+RedisStore.prototype.incrNormal = async function (ip, cb, max) {
   const key = this.key + ip
-
-  this.redis.pipeline()
+  const result = await this.redis.pipeline()
     .incr(key)
     .pttl(key)
-    .exec((err, result) => {
-    /**
-     * result[0] => incr response: [0]: error, [1]: new incr value
-     * result[1] => pttl response: [0]: error, [1]: ttl remaining
-     */
-      if (err || result[0][0] || result[1][0]) {
-        cb(err || result[0][0] || result[1][0], null)
-        return
-      }
+    .exec()
 
-      if (result[1][1] === -1) {
-        // Item just got created
-        this.redis.pexpire(key, this.timeWindow, noop)
-        cb(null, { current: 1, ttl: this.timeWindow })
-        return
-      }
+  if (err || result[0][0] || result[1][0]) {
+    cb(err || result[0][0] || result[1][0], null)
+    return
+  }
 
-      if (this.continueExceeding && result[0][1] > max) {
-        // Reset TLL if max has been exceeded and `continueExceeding` is enabled
-        this.redis.pexpire(key, this.timeWindow, noop)
-        cb(null, { current: result[0][1], ttl: this.timeWindow })
-        return
-      }
+  if (result[1][1] === -1) {
+    // Item just got created
+    this.redis.pexpire(key, this.timeWindow).catch()
+    cb(null, { current: 1, ttl: this.timeWindow })
+    return
+  }
 
-      cb(null, { current: result[0][1], ttl: result[1][1] })
-    })
+  if (this.continueExceeding && result[0][1] > max) {
+    // Reset TLL if max has been exceeded and `continueExceeding` is enabled
+    this.redis.pexpire(key, this.timeWindow).catch()
+    cb(null, { current: result[0][1], ttl: this.timeWindow })
+    return
+  }
+
+  cb(null, { current: result[0][1], ttl: result[1][1] })
 }
 
 RedisStore.prototype.child = function (routeOptions) {
   return new RedisStore(this.redis, routeOptions.timeWindow, routeOptions.continueExceeding, this.key + routeOptions.routeInfo.method + routeOptions.routeInfo.url + '-')
 }
-
-function noop () {}
 
 module.exports = RedisStore
