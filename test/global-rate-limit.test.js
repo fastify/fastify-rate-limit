e6b5096210bdd4fa7829244d46a399e8e1d960ab
Gürgün Dayıoğlu <gurgun.dayioglu@icloud.com>
2023-11-01 15:08:40 +0100

    fix test

--- test/global-rate-limit.test.js
+++ test/global-rate-limit.test.js
@@ -459,7 +459,7 @@ test('With redis store (ban)', async t => {
   res = await fastify.inject('/')
   t.equal(res.statusCode, 200)
   t.equal(res.headers['x-ratelimit-limit'], '1')
-  t.equal(res.headers['x-ratelimit-remaining'], '1')
+  t.equal(res.headers['x-ratelimit-remaining'], '0')
   t.equal(res.headers['x-ratelimit-reset'], '1')
 
   res = await fastify.inject('/')
